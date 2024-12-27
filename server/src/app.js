c0427d942e3fda8160cadc0746e4f067c5f21e1e
M-Ahmed-dev <winterfell551@gmail.com>
2024-12-27 12:43:03 +0500

    added morgan logger

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+const morgan = require("morgan");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -9,7 +11,16 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use("/planets", planetsRouter);
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+//serving file
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
