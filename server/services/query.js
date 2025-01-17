a1437d65c2ba7f9632170684e637b08ec0a4eb7a
M-Ahmed-dev <winterfell551@gmail.com>
2025-01-17 14:32:09 +0500

    added docker file and successfully deployed container

--- server/services/query.js
+++ server/services/query.js
@@ -1,5 +1,6 @@
 const DEFAULT_PAGE_LIMIT = 0; // returning all documents
 const DEFAULT_PAGE_NUMBER = 1;
+
 function getPagination(query) {
   const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
   const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
